724255d5daaaf66e1283606a5077ee5def744e78
Rogerio Hilbert Lima <rogerhil@gmail.com>
2015-01-26 14:15:05 +0000

    Some fixes after deployment

--- flaviabernardes/flaviabernardes/settings.py
+++ flaviabernardes/flaviabernardes/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.webdesign',
     'easy_thumbnails',
     'image_cropping',
     'flaviabernardes.artwork'
@@ -106,3 +107,6 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
     os.path.join(BASE_DIR, "media"),
 )
+
+from .productionsettings import *
+
