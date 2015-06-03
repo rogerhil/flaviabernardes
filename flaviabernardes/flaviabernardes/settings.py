3d739088db2e7ef901fb7d3fe9c70f7aca63d043
Rogerio Hilbert Lima <rogerhil@gmail.com>
2015-06-04 00:34:59 +0100

    Last changes before launch.

--- flaviabernardes/flaviabernardes/settings.py
+++ flaviabernardes/flaviabernardes/settings.py
@@ -132,7 +132,7 @@ CURRENT_EMAIL_MARKETING_PROVIDER = MADMIMI
 EMAIL_HOST = ""
 EMAIL_HOST_PASSWORD = ""
 EMAIL_HOST_USER = ""
-DEFAULT_FROM_EMAIL = "Flavia Bernardes - Artist " \
+DEFAULT_FROM_EMAIL = "(Flavia Bernardes - Artist) " \
                      "<flavia@flaviabernardesart.com>"
 
 try:
