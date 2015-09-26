d3ed984ede1ec5ec5733e2514c59f2f0071d1f8b
Rogerio Hilbert Lima <rogerhil@gmail.com>
2015-09-26 19:39:58 +0100

    Blog, newsletter, fixes.

--- flaviabernardes/static/js/newsletter.js
+++ flaviabernardes/static/js/newsletter.js
@@ -1,13 +1,17 @@
-
-$(window).ready(function () {
-    setupNewsletter();
-});
-
 function setupNewsletter() {
     $('#newsletter').ajaxForm({
+        beforeSubmit: function(arr, $form, options) {
+            $('#cover').fadeIn();
+        },
         success: function (response, status, xhr, $form) {
+            $('#cover').fadeOut();
             if (response.success) {
-                alert('SUCESS');
+                var email = $('#newsletter input[type=email]').val();
+                var msg = 'Please confirm your subscription in the email message that was just sent to ' + email;
+                $("#newsletter-wrap").html('<div class="center" style="color: #A50017">' + msg + '.</div>').css('padding', '5px');
+                $("#newsletter-wrap").effect('highlight', 2000, function () {
+                    $("#newsletter-wrap").effect('highlight', 2000);
+                });
             } else {
                 $('#newsletter-wrap').html(response.html);
                 setupNewsletter();
@@ -16,3 +20,5 @@ function setupNewsletter() {
         }
     });
 }
+
+setupNewsletter();
