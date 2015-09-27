d4570f5486fc3b6381a6581eb224ac7ec532202d
Rogerio Hilbert Lima <rogerhil@gmail.com>
2015-09-27 03:19:56 +0100

    Some responsive layout fixes.

--- flaviabernardes/flaviabernardes/urls.py
+++ flaviabernardes/flaviabernardes/urls.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from django.contrib import admin
 from django.views.generic import TemplateView
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.decorators import login_required, user_passes_test
 
 from .artwork.views import HomeView, PaintingsView, ArtworksSortJson
 from .blog.views import BlogView, PostView, DraftPublishView, \
@@ -11,18 +12,21 @@ from .blog.views import BlogView, PostView, DraftPublishView, \
 from .fbauth.views import LoginJson, LogoutJson
 from .newsletter.views import LandPageView, ConfirmationView, NewsletterView
 
-
+is_superuser = user_passes_test(lambda u: u.is_superuser, '/')
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'flaviabernardes.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^admin/blog/draft/(?P<pk>\d+)/publish/$',
-        csrf_exempt(DraftPublishView.as_view()), name='draft_publish'),
+        csrf_exempt(is_superuser(DraftPublishView.as_view())),
+        name='draft_publish'),
     url(r'^admin/blog/post/(?P<pk>\d+)/new-draft/$',
-        csrf_exempt(PostNewDraftView.as_view()), name='blog_post_new_draft'),
+        csrf_exempt(is_superuser(PostNewDraftView.as_view())),
+        name='blog_post_new_draft'),
     url(r'^admin/blog/draft/(?P<pk>\d+)/preview/$',
-        csrf_exempt(DraftPreview.as_view()), name='draft_preview'),
+        csrf_exempt(is_superuser(DraftPreview.as_view())),
+        name='draft_preview'),
     url(r'^admin/', include(admin.site.urls)),
     #url(r'^login/json/$', LoginJson.as_view(), name='login'),
     #url(r'^logout/json/$', LogoutJson.as_view(), name='logout'),
