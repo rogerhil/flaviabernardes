430929d349479f9d7ef0ebe19c05910ee08cb6b8
Rogerio Hilbert Lima <rogerhil@gmail.com>
2015-09-27 04:23:13 +0100

    Fix fix on scroll and blog posts page responsive layout.

--- flaviabernardes/static/js/fixatscroll.js
+++ flaviabernardes/static/js/fixatscroll.js
@@ -1,23 +1,29 @@
 $(window).load(function () {
     function scrollFixedElements() {
         var $sm = $('.scroll-fixed');
-        var stop = $('#' + $sm.attr('anchor')).offset().top;
+        var $anchor = $('#' + $sm.attr('anchor'));
+        var height = $sm.css('height');
+        var margin = $sm.css('margin');
+        var stop = $anchor.offset().top;
         var wtop = $(window).scrollTop();
         var fixat = Number($sm.attr('fixat'));
         if ((wtop + fixat) > stop) {
             $sm.css('position', 'fixed');
             $sm.css('top', fixat + 'px');
+            $anchor.css('height', height);
+            $anchor.css('margin', margin);
         } else {
             $sm.css('position', 'static');
             $sm.css('top', '0px');
+            $anchor.css('height', 0);
+            $anchor.css('margin', 0);
         }
     }
     scrollFixedElements();
     $(window).scroll(function () {
-        scrollFixedElements()
+        scrollFixedElements();
     });
     $(window).resize(function () {
-        scrollFixedElements()
+        scrollFixedElements();
     });
-
 });
