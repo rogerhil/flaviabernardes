49b2d0a51d5fcb822cbe6a2f153799326f45ab9e
Rogerio Hilbert Lima <rogerhil@gmail.com>
2015-10-14 17:59:54 +0100

    Parallax in artworks test.

--- flaviabernardes/static/js/parallax2.js
+++ flaviabernardes/static/js/parallax2.js
@@ -3,24 +3,25 @@ var headerBgColor;
 $(window).ready(function () {
     var isiOS = /iPhone|iPad|iPod/i.test(navigator.userAgent);
     headerBgColor = $('#header').css('background-color');
-    $(".banner").css("background-attachment", "fixed");
+    $("#artworks .parallax").css("background-attachment", "fixed");
     if (isiOS) {
         //parallaxPosition();
         return;
     }
-    $(window).resize(parallaxPosition);
-    $(window).scroll(parallaxPosition);
-    parallaxPosition();
+    $(window).resize(parallaxPosition2);
+    $(window).scroll(parallaxPosition2);
+    parallaxPosition2();
 });
 
-function parallaxPosition(e) {
+function parallaxPosition2(e) {
     //var topWindow = $(window).scrollTop();
     //var widthWindow = $(window).width();
     var headerHeight = Number($('#header').css('height').replace('px', ''));
-    $(".banner").each(function(i) {
+    $("#artworks .parallax").each(function(i) {
         var topWindow = $(window).scrollTop();
         //var bottomWindow = $(window).scrollBottom();
         var widthWindow = $(window).width();
+        var heightWindow = $(window).height();
         if (!$(this).attr('bg')) {
             $(this).attr('bg', $(this).css('background-image'));
         }
@@ -30,14 +31,20 @@ function parallaxPosition(e) {
         var h = bgSize.split('x')[1];
         var horp = (- (w - widthWindow) / 2) + 'px';
         var height = Number($(this).position().top);
-        var value = headerHeight / 2 + height / 2 - (topWindow / 2);
+        var value = headerHeight / 2 + height / 2 - (topWindow / 6);
         if (widthWindow > w) {
             horp = '0px';
-            $(this).css("background-size", "cover");
+            //$(this).css("background-size", "cover");
         }
-        $(this).css("background-position", horp + " " + value + "px");
+        horp = $(this).parent().position().left;
+        horp = horp + ((Number($(this).css('width').replace('px', '')) - w) / 2);
+        var hh = Number($(this).parent().position().top) - topWindow;
+        //if (hh < heightWindow)
+        //hh = hh + hh * 0.5;
+        $(this).css("background-position", horp + "px " + (value + hh) + "px");
         if (widthWindow > w) {
-            $(this).css("background-size", widthWindow + "px");
+            //$(this).css("background-size", widthWindow + "px");
         }
+        //$(this).css("background-size", "640px 600px");
     });
 }
